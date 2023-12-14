74827e816a8bff7e13de0bb322eaa7fd298fefb3
Francisco Ezequiel Baños Ramirez <chekelosos@gmail.com>
2023-12-14 10:31:26 -0600

    Added main functionality and styles for the drumkey component

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,34 @@
 <script>
+    import DrumKey from "./lib/DrumKey.svelte";
+    class buttonObj {
+        constructor (key, name){
+            this.key = key
+            this.audio = new Audio(`https://s3.amazonaws.com/freecodecamp/drums/${name}.mp3`)
+            this.name = name
+        }
+    }
+
+    const heaterButtons = [
+        new buttonObj('q', 'Heater-1'),
+        new buttonObj('w', 'Heater-2'),
+        new buttonObj('e', 'Heater-3'),
+        new buttonObj('a', 'Heater-4_1'),
+        new buttonObj('s', 'Heater-6'),
+        new buttonObj('d', 'Dsc_Oh'),
+        new buttonObj('z', 'Kick_n_Hat'),
+        new buttonObj('x', 'RP4_KICK_1'),
+        new buttonObj('c', 'Cev_H2')
+    ]
 </script>
 
 <main>
     <div id="general-container">
         <div id="drum-machine">
-            
+            <div class="buttons-container">
+            {#each heaterButtons as button}
+                <DrumKey {...button}/>
+            {/each}
+            </div>
         </div>
     </div>
 </main>
@@ -23,4 +47,13 @@
             text-align: center
             background-color: #b3b3b3
             min-height: 316px
+            display: grid
+            grid-template-columns: 1.5fr 1fr
+            .buttons-container
+                width: 332px
+                height:  272px
+                margin: auto auto
+                display: grid
+                gap: 0.5rem
+                grid-template-columns: repeat(3, 1fr) 
 </style>
---
+++ src/lib/DrumKey.svelte
@@ -0,0 +1,27 @@
+<script>
+    export let key;
+    export let audio;
+    export let name;
+    function handleKeyDown(e){
+        if (e.key.toLowerCase() == key.toLowerCase()){
+            audio.play()
+        }
+    }
+</script>
+
+<svelte:window on:keydown={handleKeyDown}/>
+<button class="drum-machine-key" id={name} on:click={audio.play()}>{key.toUpperCase()}</button>
+
+<style lang="sass">
+    .drum-machine-key
+        background-color: grey
+        border: 0
+        box-shadow: black 3px 3px 5px
+        border-radius: 5px
+        text-align: center
+        font-size: 1.5rem
+        font-weight: bold
+        width: 100px
+        height: 80px
+        cursor: pointer
+</style>
