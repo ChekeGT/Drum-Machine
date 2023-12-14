e0efd144ef78c02ad61853cf1cf90c12bf71314e
Francisco Ezequiel Baños Ramirez <chekelosos@gmail.com>
2023-12-14 12:40:47 -0600

    Added animation to the drumKey component

--- src/lib/DrumKey.svelte
+++ src/lib/DrumKey.svelte
@@ -2,16 +2,36 @@
     export let key;
     export let audio;
     export let name;
+    let animation = false;
+    function isPlaying(audio){
+        return !audio.paused
+    }
+    function makeAnimation(){        
+        animation = true
+        setTimeout(() => {
+            animation = false
+        }, 75)
+    }
+    function playAudio(){
+        if (isPlaying(audio)){
+            audio.currentTime = 0
+        }
+        audio.play()
+    }
     function handleKeyDown(e){
         if (e.key.toLowerCase() == key.toLowerCase()){
-            audio.play()
+            makeAnimation()
+           playAudio()
         }
+    }
+    function handleClick(){
+        makeAnimation()
+        playAudio()
     }
 </script>
 
 <svelte:window on:keydown={handleKeyDown}/>
-<button class="drum-machine-key" id={name} on:click={audio.play()}>{key.toUpperCase()}</button>
-
+<button class={`drum-machine-key ${animation ? 'animation' : ''}`} id={name} on:click={handleClick}>{key.toUpperCase()}</button>
 <style lang="sass">
     .drum-machine-key
         background-color: grey
@@ -24,4 +44,10 @@
         width: 100px
         height: 80px
         cursor: pointer
+    .animation
+        background-color: orange
+        height: 77px
+        margin-top: 3px
+        box-shadow: orange 0px 3px
+    
 </style>
